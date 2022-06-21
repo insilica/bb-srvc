b5074fbe920fe2e3410a944b2fe9eb60839e0f4d
John Shaffer <john@insilica.co>
2022-06-21 16:14:33 -0500

    Version 0.2.0-SNAPSHOT

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
   (:require [org.corfield.build :as bb]))
 
 (def lib 'co.insilica/bb-srvc)
-(def version "0.1.0")
+(def version "0.2.0")
 (defn get-version [opts]
   (str version (when (:snapshot opts) "-SNAPSHOT")))
 
