7373fea80948edaaf5d15e95a93dc22483d19421
John Shaffer <john@insilica.co>
2022-06-21 14:22:07 -0500

    Rename hash to json-hash

--- src/srvc/bb.clj
+++ src/srvc/bb.clj
@@ -1,11 +1,10 @@
 (ns srvc.bb
-  (:refer-clojure :exclude [hash])
   (:require [insilica.canonical-json :as json]
             [multihash.core :as multihash]
             [multihash.digest :as digest]))
 
-(defn hash [m]
+(defn json-hash [m]
   (-> (dissoc m :hash :meta) json/write-str digest/sha2-256 multihash/base58))
 
 (defn add-hash [m]
-  (assoc m :hash (hash m)))
+  (assoc m :hash (json-hash m)))
