5b745f053e192506b93358e9af859a83b1ec1c29
John Shaffer <john@insilica.co>
2022-07-07 13:24:48 -0500

    Add a separate jar step to build.clj

--- build.clj
+++ build.clj
@@ -9,18 +9,20 @@
 
 (defn test [opts]
   (-> opts
+      bb/clean
       (assoc :main-args ["-m" "kaocha.runner"])
       (bb/run-task [:test]))
   opts)
 
-(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
+(defn jar [opts]
   (-> opts
       (assoc :lib lib :version (get-version opts))
       bb/clean
-      test
-      bb/clean
       (assoc :src-pom "template/pom.xml")
       bb/jar))
+
+(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
+  (-> opts test jar))
 
 (defn deploy "Deploy the JAR to Clojars." [opts]
   (-> opts
