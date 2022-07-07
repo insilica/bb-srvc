965baf5ddc5efee15138681cd91e104223f6d992
John Shaffer <john@insilica.co>
2022-07-07 13:27:14 -0500

    Add a nix flake build to produce a graalvm binary

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A clj-nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    clj-nix = {
+      url = "github:jlesquembre/clj-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, clj-nix }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        cljpkgs = clj-nix.packages."${system}";
+      in
+
+      {
+        packages = {
+
+          clj = cljpkgs.mkCljBin {
+            projectSrc = ./.;
+            name = "co.insilica/bb-srvc";
+            main-ns = "srvc.bb.cli";
+            jdkRunner = pkgs.jdk17_headless;
+          };
+
+          graalBin = cljpkgs.mkGraalBin {
+            cljDrv = self.packages."${system}".clj;
+          };
+
+        };
+      });
+
+}
